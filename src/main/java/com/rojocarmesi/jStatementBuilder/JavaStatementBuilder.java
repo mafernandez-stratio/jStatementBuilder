f0e1004aeb2b9e54eccb01aad854fd0a683e2602
miguel0afd <mafernandez@stratio.com>
2014-11-10 21:20:18 +0100

    Starting to use Java Custom Annotations

--- src/main/java/com/rojocarmesi/jStatementBuilder/JavaStatementBuilder.java
+++ src/main/java/com/rojocarmesi/jStatementBuilder/JavaStatementBuilder.java
@@ -1,12 +1,26 @@
 package com.rojocarmesi.jStatementBuilder;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Java Statement Builder
  *
+ * Use this annotation to trigger the code generation of a fluent pojo builder.
  */
-public class JavaStatementBuilder{
-    public static void main(String[] args)
-    {
-        System.out.println("Java Statement Builder!");
-    }
+@Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.TYPE, ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+public @interface JavaStatementBuilder{
+    public final String DEFAULT_NAME = "*Builder";
+    public final String DEFAULT_PACKAGE = "*";
+    public final String DEFAULT_SETTER_NAME = "with*";
+
+    /**
+     * Specifies the super class of the builder.
+     *
+     * @return the super class of the builder
+     */
+    Class<?> withSuperClass() default Object.class;
 }
